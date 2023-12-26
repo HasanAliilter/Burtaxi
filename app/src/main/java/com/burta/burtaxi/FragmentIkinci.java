7fc3604b9e405727f66036f53c259b405bc2e4f5
Hasan Ali İlter <hasanilter003@gmail.com>
2023-12-26 18:53:29 +0300

    Yorum Sayfasinin yapilmasi

--- app/src/main/java/com/burta/burtaxi/FragmentIkinci.java
+++ app/src/main/java/com/burta/burtaxi/FragmentIkinci.java
@@ -13,11 +13,20 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class FragmentIkinci extends Fragment {
     private Toolbar toolbar_yorumlar;
     private RecyclerView rv_yorumlar;
+    private DatabaseReference yorumlarRef;
+    private ArrayList<Yorumlar> yorumlarArrayList;
+    private YorumlarAdapter adapter;
 
 
     @Nullable
@@ -34,10 +43,34 @@ public class FragmentIkinci extends Fragment {
         rv_yorumlar.setHasFixedSize(true);
         rv_yorumlar.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        yorumlarArrayList = new ArrayList<>();
+        adapter = new YorumlarAdapter(rootView.getContext(), yorumlarArrayList);
 
+        yorumlarRef = FirebaseDatabase.getInstance().getReference().child("yorumlar");
+
+        yorumlarRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                yorumlarArrayList.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Yorumlar yorumlar = snapshot.getValue(Yorumlar.class);
+                    yorumlarArrayList.add(yorumlar);
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Hata durumu
+            }
+        });
+
+        rv_yorumlar.setAdapter(adapter);
 
         return rootView;
 
-
     }
+
 }
